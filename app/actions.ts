948a854af1b24ecc842b2ea7fcf77b3139bef327
Nelson Rojas <nelson.rojas@bpforms.com>
2024-06-08 18:23:45 -0400

    Set up fetch data from API

---
+++ app/actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+export async function fetchDogs(breeds: string, numberOfDogs: number) {
+  try {
+    const response = await fetch(
+      `https://dog.ceo/api/breed/${breeds}images/random/${numberOfDogs}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response for fetching dogs was not ok");
+    }
+    const data = await response.json();
+    console.log("** DOGS **", data.message);
+    return data.message;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function fetchBreeds() {
+  try {
+    const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
+    if (!response.ok) {
+      throw new Error("Network response for fetching dogs was not ok");
+    }
+    const data = await response.json();
+    console.log("** BREEDS **", data.message);
+    return data.message;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
